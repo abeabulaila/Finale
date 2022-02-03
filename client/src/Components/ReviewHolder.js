a138c8ac9457e22660beb90ae21b3d2060322e9a
Ibrahim Abulaila <abeabulaila@gmail.com>
2022-02-03 14:44:14 -0500

    succesful embedding, styling for review/song divs

--- client/src/Components/ReviewHolder.js
+++ client/src/Components/ReviewHolder.js
@@ -5,7 +5,7 @@ function ReviewHolder(){
     return(
         <div className="review-div">
             <h2> Review Here </h2>
-            <div style={{ borderTop: "2.5px solid #FF0000 ", marginRight: 100 }}></div>
+            <div style={{ borderTop: "2.5px solid #FF0000 ", marginRight: 100, marginBottom: "10px" }}></div>
             <div>
                 <p> this is a review and its a good one i really enjoy this band a lot i like them to play and stuff</p>
             </div>
--- client/src/Components/SongHolder.js
+++ client/src/Components/SongHolder.js
@@ -1,14 +1,22 @@
 import React from "react";
 
+
 function SongHolder() {
 
+    function show(){
+        return (
+            <iframe style={{border: 0, width: "100%", height: "120px"}} src="https://bandcamp.com/EmbeddedPlayer/album=1630100310/size=large/bgcol=333333/linkcol=9a64ff/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://retailband.bandcamp.com/album/dead">DEAD by RETAIL</a></iframe>
+
+        )
+    }
+
     return (
-        <div className="review-div">
-            <h2> Retail's Latest Single </h2>
-            <div style={{ borderTop: "2.5px solid #FF0000 ", marginRight: 100 }}></div>
-            {/* <div>
-                <iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=1630100310/size=large/bgcol=333333/linkcol=9a64ff/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://retailband.bandcamp.com/album/dead">DEAD by RETAIL</a></iframe>
-            </div> */}
+        <div className="song-div">
+            <h2> Retail - Dead</h2>
+            <div style={{ borderTop: "2.5px solid #FF0000 ", marginRight: 100, marginBottom: "10px" }}></div>
+           <div>
+               {show()}
+           </div>
         </div>
     )
 }
