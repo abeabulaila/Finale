f179a024938b6e1901130ddc2abad46b36816f44
Ibrahim Abulaila <abeabulaila@gmail.com>
2022-02-04 15:30:05 -0500

    simple artists styling

--- client/src/Components/BandCard.js
+++ client/src/Components/BandCard.js
@@ -4,9 +4,9 @@ function BandCard({ bandName, album, genre, img }) {
 
 
     return (
-        <div>
+        <div className="band-div">
             <h2>{bandName}</h2>
-            <img src={img} />
+            <img className="band-img" src={img} />
             <h4>{album}</h4>
             <h5>{genre}</h5>
         </div>
